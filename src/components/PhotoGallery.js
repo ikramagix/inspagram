419f0fe778ada94801ed42a07d97f7b89857d403
ikramiste <ikrame.saadi@gmail.com>
2024-02-01 19:46:45 +0100

    in need of prod license

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -3,56 +3,66 @@ import unsplash from '../api/unsplash';
 import ReactModal from 'react-modal';
 
 const PhotoGallery = () => {
-    const [photos, setPhotos] = useState([]);
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [photos, setPhotos] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
 
-    useEffect(() => {
-        // Charger des photos aléatoires
-        unsplash.photos.getRandom({ count: 10 }) // Obtenir 10 photos aléatoires
-            .then(result => {
-                if (result.errors) {
-                    console.log('error occurred: ', result.errors[0]);
-                } else {
-                    setPhotos(result.response);
-                }
-            });
-    }, []);
-
-    const openModal = (photo) => {
-        setSelectedPhoto(photo);
-        setModalIsOpen(true);
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      try {
+        // Charger 5 photos
+        const response = await unsplash.photos.list({ page: 1, perPage: 5 });
+        if (response.errors) {
+          console.log('error occurred: ', response.errors[0]);
+        } else {
+          setPhotos(response.response);
+        }
+      } catch (error) {
+        console.error('Error fetching photos:', error);
+      }
     };
 
-    const closeModal = () => {
-        setModalIsOpen(false);
-        setSelectedPhoto(null);
-    };
+    fetchPhotos();
+  }, []);
 
-    return (
-        <div className="container my-4">
-            <div className="row mt-4">
-                {photos.map((photo, index) => (
-                    <div className="col-md-4 mb-3" key={index} onClick={() => openModal(photo)}>
-                        <img
-                            src={photo.urls.small}
-                            alt={photo.alt_description}
-                            className="img-fluid rounded"
-                        />
-                    </div>
-                ))}
-            </div>
+  const openModal = (photo) => {
+    setSelectedPhoto(photo);
+    setModalIsOpen(true);
+  };
 
-            <ReactModal isOpen={modalIsOpen} onRequestClose={closeModal}>
-                {selectedPhoto && (
-                    <div>
-                        <img src={selectedPhoto.urls.regular} alt={selectedPhoto.alt_description} className="img-fluid rounded" />
-                        <p className="mt-3">{selectedPhoto.description || 'No Description'}</p>
-                    </div>
-                )}
-            </ReactModal>
-        </div>
-    );
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setSelectedPhoto(null);
+  };
+
+  return (
+    <div className="container my-4">
+      <div className="row mt-4">
+        {photos.map((photo, index) => (
+          <div className="col-md-4 mb-3" key={index} onClick={() => openModal(photo)}>
+            <img
+              src={photo.urls.small}
+              alt={photo.alt_description}
+              className="img-fluid rounded"
+            />
+          </div>
+        ))}
+      </div>
+
+      <ReactModal isOpen={modalIsOpen} onRequestClose={closeModal}>
+        {selectedPhoto && (
+          <div>
+            <img
+              src={selectedPhoto.urls.regular}
+              alt={selectedPhoto.alt_description}
+              className="img-fluid rounded"
+            />
+            <p className="mt-3">{selectedPhoto.description || 'No Description'}</p>
+          </div>
+        )}
+      </ReactModal>
+    </div>
+  );
 };
 
 export default PhotoGallery;
