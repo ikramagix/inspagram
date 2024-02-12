b8ef5212efba38ab047fcc102bc9832b868328f7
Ikrame Saadi <146087805+ikramagic@users.noreply.github.com>
2024-02-12 11:50:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './components/PhotoGallery.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{ fontFamily: "'Sixtyfour', sans-serif" }} className="fluorescent-text">ikstagram</h1>
-      <h2 style={{ fontFamily: "'Sixtyfour', sans-serif" }} className="fluorescent-text">@aie_aie_art</h2>
+      <h1 style={{ fontFamily: "'Sixtyfour', sans-serif" }} className="fluorescent-text">inspagram</h1>
+      <h2 style={{ fontFamily: "'Sixtyfour', sans-serif" }} className="fluorescent-text">retrouvez-moi sur @aie_aie_art</h2>
       <PhotoGallery />
     </div>
   );
