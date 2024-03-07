ee2b6cd00bb114ee784404780b9f7fc6090d42e1
ikramiste <ikrame.saadi@gmail.com>
2024-03-07 12:54:47 +0200

    fix

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,7 +1,7 @@
 import { createApi } from 'unsplash-js';
 
 const unsplash = createApi({
-  accessKey: process.env.REACT_APP_UNSPLASH_ACCESS_KEY, 
+  REACT_APP_UNSPLASH_API_KEY: process.env.REACT_APP_UNSPLASH_ACCESS_KEY, 
 });
 
 export default unsplash;
