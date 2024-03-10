c9768abf205abd41a04501406807e2d400d4378f
ikramiste <ikrame.saadi@gmail.com>
2024-03-11 01:01:15 +0200

    minor fixes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "buffer";
 function App() {
   return (
     <div className="App">
-      <h1 style={{ fontFamily: "'Sixtyfour', sans-serif" }} className="fluorescent-text">inspagram's manifest</h1>
+      <h1 style={{ fontFamily: "'Sixtyfour', sans-serif" }} className="fluorescent-text">inspagram's:manifest | Get inspired | Unsplash API Random Image Generator</h1>
       <h2 style={{ fontFamily: "'Madimi One', sans-serif" }} className="fluorescent-text">random is beautiful | discover artists | explore your imagination | find inspiration</h2>
       <TiltedPageScroll />
       <PhotoGallery />
